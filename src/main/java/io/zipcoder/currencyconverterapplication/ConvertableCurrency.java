b1489e132a719b50ee02c6a22e8d52a7ab7e6938
Jorden Ross <JRoss9117@gmail.com>
2024-04-17 11:25:07 -0400

    Psuedo coding

--- src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
@@ -5,3 +5,22 @@ public interface ConvertableCurrency {
         return Double.MAX_VALUE;
     }
 }
+
+//Identify exchange rates between any two currency types
+    /*
+    double a = currencyType
+     double b = exchangeRate
+     double c = newCurrencyValue
+     double a * b = c && a = c/b
+     */
+
+
+//Use Object orientation and enums
+    /*
+    ENUMS used for each countries currency
+    */
+//can not waste any value during exchange
+    /*
+    Using double data type for more accurate values
+    */
+
--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
@@ -2,6 +2,23 @@ package io.zipcoder.currencyconverterapplication;
 
 public class CurrencyConverter {
     public static Double convert(Double amountOfBaseCurrency, ConvertableCurrency sourceCurrencyType, CurrencyType targetCurrencyType) {
+    /*
+    double a = sourceCurrencyType
+     double b = amountOfBaseCurrency
+     double c = targetCurrencyType
+     double a * b = c || a = c/b
+
+     double amountOfBaseCurrency = targetCurrency/sourceCurrencyType
+     */
+/*
+ CurrencyType targetCurrencyType = ENUM value we are comparing against
+ ConvertableCurrency sourceCurrencyType = ENUM value we are starting with
+ double amountOfBaseCurrency = money
+
+*/
+
+
+
         return sourceCurrencyType.convert(targetCurrencyType) * amountOfBaseCurrency;
     }
 }
